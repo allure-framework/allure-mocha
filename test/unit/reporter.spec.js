c42bfbd2c696f938a3a1e55903b358404a5e7869
Alexander Deschamps <adeschamps@shutterstock.com>
2016-02-28 23:18:29 -0500

    Accounting for error's in the before hook
    Why:
    
    * previously, tests seemed to immediately exit with no status code (Defaulted to 0) when there was an error in a before block
    * this was hapening because of an unhandled error where there was no current step when a before hook failed
    
    This change addresses the need by:
    
    * I added validation where if there was no current test, it would use the title of the one that failed (which happens to explicitly say the before hook)

--- test/unit/reporter.spec.js
+++ test/unit/reporter.spec.js
@@ -8,39 +8,53 @@ var path = require('path'),
 
 chai.use(sinonChai);
 
-mockery.enable({
-    warnOnUnregistered: false,
-    useCleanCache: true
-});
-
-var allureMock = sinon.stub({
-        setOptions: function() {},
-        startSuite: function() {},
-        endSuite: function() {},
-        getSuite: function() {},
-        startCase: function() {},
-        endCase: function() {},
-        pendingCase: function() {}
-    }),
-    runtimeMock = sinon.stub({
-        createStep: function() {},
-        createAttachment: function() {},
-        addLabel: function() {}
-    });
-runtimeMock.createStep.returns(function() {});
-runtimeMock.createAttachment.returns(function() {});
-
-
-mockery.registerMock('allure-js-commons', function() {
-    return allureMock;
-});
-mockery.registerMock('allure-js-commons/runtime', function() {
-    return runtimeMock;
-});
-
-
-var reporter = require('../../');
 describe("Allure reporter", function() {
+    var allureMock;
+    var runtimeMock;
+    var sandbox;
+    var reporter;
+
+    beforeEach(function(){
+        sandbox = sinon.sandbox.create();
+        mockery.enable({
+            warnOnUnregistered: false,
+            warnOnReplace: false,
+            useCleanCache: true
+        });
+        allureMock = sandbox.stub({
+                getCurrentTest: function(){},
+                setOptions: function() {},
+                startSuite: function() {},
+                endSuite: function() {},
+                getSuite: function() {},
+                startCase: function() {},
+                endCase: function() {},
+                pendingCase: function() {}
+            })
+        allureMock.getCurrentTest.returns('a test');
+        runtimeMock = sandbox.stub({
+                createStep: function() {},
+                createAttachment: function() {},
+                addLabel: function() {}
+            });
+        runtimeMock.createStep.returns(function() {});
+        runtimeMock.createAttachment.returns(function() {});
+
+
+        mockery.registerMock('allure-js-commons', function() {
+            return allureMock;
+        });
+        mockery.registerMock('allure-js-commons/runtime', function() {
+            return runtimeMock;
+        });
+        reporter = require('../../');
+    })
+
+    afterEach(function(){
+        sandbox.restore();
+        mockery.disable();
+    })
+
     it("should report test results", function(done) {
         var mocha = new Mocha({
             reporter: reporter
@@ -64,6 +78,29 @@ describe("Allure reporter", function() {
 
             expect(allureMock.pendingCase).calledOnce;
             expect(allureMock.pendingCase.firstCall).calledWith('pending test');
+            done();
+        })
+    });
+    it("should report test results if there is a failure in a before test hook", function(done) {
+        allureMock.getCurrentTest.onFirstCall().returns(undefined);
+        allureMock.getCurrentTest.onSecondCall().returns('a test');
+        var mocha = new Mocha({
+            reporter: reporter
+        });
+        mocha.addFile(path.join(__dirname, '../fixtures/before_all.spec.js'));
+        mocha.run(function(status) {
+            expect(allureMock.startSuite).callCount(4);
+            expect(allureMock.startSuite.secondCall).calledWithExactly('Before all tests');
+            expect(allureMock.startSuite.thirdCall).calledWithExactly('Before all tests broken before');
+            expect(allureMock.startSuite.lastCall).calledWithExactly('Before all tests before not broken');
+            expect(allureMock.endSuite).callCount(4);
+
+            expect(allureMock.startCase.firstCall).calledAfter(allureMock.startSuite.secondCall);
+            expect(allureMock.startCase.firstCall).calledWithExactly('"before all" hook');
+            expect(allureMock.endCase.firstCall).calledWith('broken', sinon.match.instanceOf(Error));
+
+            expect(allureMock.startCase.secondCall).calledWithExactly('a test');
+            expect(allureMock.endCase).to.have.been.calledTwice;
 
             done();
         })
